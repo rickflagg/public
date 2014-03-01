0ba8c39065afdef8a4f02bb1c676371eba5859fb
Rick Flagg <rick.flagg@gmail.com>
2014-03-01 18:05:18 -0500

    Fleshing out SQL structure

--- python/FairfaxOffenders/resources/sql/Sprint_1.sql
+++ python/FairfaxOffenders/resources/sql/Sprint_1.sql
@@ -70,7 +70,7 @@ CREATE TABLE offenders
   CONSTRAINT pk_offenders PRIMARY KEY (id)
 );
 
-DROP TABLE IF EXISTS state_statutes;
+DROP TABLE IF EXISTS state_statutes  CASCADE;
 DROP SEQUENCE IF EXISTS seq_state_statutes;
 
 CREATE TABLE state_statutes
@@ -83,7 +83,7 @@ CREATE TABLE state_statutes
   CONSTRAINT fk_statutes_to_states FOREIGN KEY (state_id) REFERENCES offender_states
 );
 
-DROP TABLE IF EXISTS offences;
+DROP TABLE IF EXISTS offences  CASCADE;
 DROP SEQUENCE IF EXISTS seq_offences;
 
 CREATE SEQUENCE seq_offences;
@@ -101,7 +101,67 @@ CREATE TABLE offences
   CONSTRAINT fk_offences_to_areas FOREIGN KEY (area_id) REFERENCES offender_area(id)
 );
 
+CREATE OR REPLACE FUNCTION fn_save_offence
+(
+   IN prmLastname offenders.last_name%TYPE,
+   IN prmFirstname offenders.first_name%TYPE,
+   IN prmMiddlename offenders.middle_name%TYPE,
+   IN prmAge offenders.age%TYPE,
+   IN prmDateArrested offences.offence_date%TYPE,
+   IN prmChargeStatute state_statutes.code%TYPE,
+   IN prmChargeDescription state_statutes.description%TYPE,
+   IN prmAddress offenders.address%TYPE,
+   IN prmLatitude offenders.latitude%TYPE,
+   IN prmLongitude offenders.longitude%TYPE,
+   IN prmState offender_states.abbreviation%TYPE,
+   IN prmArea offender_area.county%TYPE
+)
+RETURNS VOID
+AS
+$BODY$
+DECLARE
+  state_id numeric;
 
+BEGIN
+  -- Check to see if the state exists
+  SELECT id INTO state_id FROM offender_states WHERE abbreviation = prmState;
+
+  IF state_id is null THEN
+    INSERT INTO offender_states
+    (
+      id,
+      abbreviation
+    )
+    VALUES
+    (
+      nextval('seq_offender_states'),
+      prmState
+    );
+
+    state_id = currval('seq_offender_states');
+
+  END IF;
+
+END
+
+$BODY$
+LANGUAGE plpgsql;
+
+ALTER FUNCTION fn_save_offence
+(
+   offenders.last_name%TYPE,
+   offenders.first_name%TYPE,
+   offenders.middle_name%TYPE,
+   offenders.age%TYPE,
+   offences.offence_date%TYPE,
+   state_statutes.code%TYPE,
+   state_statutes.description%TYPE,
+   offenders.address%TYPE,
+   offenders.latitude%TYPE,
+   offenders.longitude%TYPE,
+   offender_states.abbreviation%TYPE,
+   offender_area.county%TYPE
+) OWNER TO criminal_offenders_user;
 
 
 
