2368f5b49c4e932cacaf0e6fbb30c3d0d7c916a1
Rick Flagg <rick.flagg@gmail.com>
2014-03-02 16:27:12 -0500

    Adding Oracle stub implementation

---
+++ sharp/FlaggLib4Net/FlaggLib4Net.Data.Oracle/Extensions/DataCommandExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlaggLib4Net.Data.Oracle.Extensions
+{
+    public static class DataCommandExtensions
+    {
+
+    }
+}
