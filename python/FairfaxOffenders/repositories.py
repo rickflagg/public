1b93a7e8b31e8440306459e92e8f93f1b95445dc
Rick Flagg <rick.flagg@gmail.com>
2014-03-06 07:32:42 -0500

    Refactoring method names to comply to PEP8 Added base method to convert dictionary to list for parameters

--- python/FairfaxOffenders/repositories.py
+++ python/FairfaxOffenders/repositories.py
@@ -3,12 +3,13 @@ __author__ = "Rick Flagg"
 import psycopg2
 from utilities import *
 
+
 class BaseRepository(object):
 
     def __init__(self):
         pass
 
-    def getConnectionString(self):
+    def connection_string(self):
 
         config = ConfigurationManager()
 
@@ -19,25 +20,25 @@ class BaseRepository(object):
 
         return "host='{0}' dbname='{1}' user='{2}' password='{3}'".format(host, database, user, password)
 
-    def getConnection(self):
+    def connection(self):
 
         try:
 
-            self._cn = psycopg2.connect(self.getConnectionString())
+            self._cn = psycopg2.connect(self.connection_string())
             return self._cn
 
         except psycopg2.DatabaseError, e:
 
             return 'Error %s' % e
 
-    def executeProcedure(self, procName, params=None):
+    def execute_refcursor_function(self, procName, params=None):
 
         proc_params = ['refcursor']
 
         if params is not None:
             proc_params = proc_params + params
 
-        cn = self.getConnection()
+        cn = self.connection()
         cur1 = cn.cursor()
         cur1.callproc(procName, proc_params)
         cur2 = cn.cursor('refcursor')
@@ -45,15 +46,20 @@ class BaseRepository(object):
 
         return cur2.fetchall()
 
+    def execute_function(self, procName, params=None):
 
-    def executeResult(self, sql, params=None):
+        cn = self.connection()
+        cur = cn.cursor()
+        cur.callproc(procName, params)
+
+    def execute_result(self, sql, params=None):
 
         cn = None
         cur = None
         retval = None
 
         try:
-            cn = self.getConnection()
+            cn = self.connection()
             cur = cn.cursor()
 
             if ConfigurationManager().readBooleanProperty("general", "show.sql") is True:
@@ -73,13 +79,13 @@ class BaseRepository(object):
             if cn is not None:
                 cn.close()
 
-    def executeInsertOrUpdate(self, sql, params=None):
+    def execute_insert_or_update(self, sql, params=None):
 
         cn = None
         cur = None
 
         try:
-            cn = self.getConnection()
+            cn = self.connection()
             cur = cn.cursor()
             cur.execute(sql, params)
 
@@ -94,3 +100,24 @@ class BaseRepository(object):
 
             if cn is not None:
                 cn.close()
+
+    def dictionary_to_parameters(self, dict=None):
+
+        retval = []
+
+        if dict.hasKeys():
+            for key, value in dict.iteritems():
+                retval.append(value)
+
+        return retval
+
+class OffenceRepository(BaseRepository):
+
+    def __init__(self):
+        super(BaseRepository, self).__init__()
+
+    def save_offence(self, values={}):
+
+        if values.has_keys():
+            self.executeNoResultFunction("", self.dictionary_to_parameters(values) )
+
