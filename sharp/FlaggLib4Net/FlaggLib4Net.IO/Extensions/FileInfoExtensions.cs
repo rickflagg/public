b87d0298204622eedd7398a59b6a9c91f5e4f71f
Rick Flagg <rick.flagg@gmail.com>
2014-05-14 04:32:06 -0400

    Added Concatenate File Extension Method for IList<FileInfo>. Added TestSetting file

--- sharp/FlaggLib4Net/FlaggLib4Net.IO/Extensions/FileInfoExtensions.cs
+++ sharp/FlaggLib4Net/FlaggLib4Net.IO/Extensions/FileInfoExtensions.cs
@@ -10,6 +10,66 @@ namespace FlaggLib4Net.IO.Extensions
 {
     public static class FileInfoExtensions
     {
+
+        public static byte[] ConcatenateFiles(this IList<FileInfo> files)
+        {
+            byte[] retval = null;
+            long totalBytes = 0;
+            int positionIndicator = 0;
+
+            if (files.AllFilesExist())
+            {
+                IList<byte[]> fileBytes = new List<byte[]>();
+
+                foreach (FileInfo file in files)
+                {
+                    byte[] tempBytes = File.ReadAllBytes(file.FullName);
+                    totalBytes += tempBytes.LongLength;
+                    fileBytes.Add(tempBytes);
+                }
+
+                retval = new byte[totalBytes];
+
+                for (int i = 0; i < files.Count; i++)
+                {
+                    byte[] tempBytes = fileBytes[i];
+
+                    Buffer.BlockCopy(tempBytes, 0, retval, positionIndicator, tempBytes.Length);
+
+                    positionIndicator += tempBytes.Length;
+
+                }
+
+            }
+
+
+            return retval;
+        }
+
+
+        public static bool AllFilesExist(this IList<FileInfo> files)
+        {
+            bool retval = true;
+
+            if (files == null || files.Count == 0)
+            {
+                retval = false;
+            }
+            else
+            {
+                foreach (FileInfo file in files)
+                {
+                    if (!file.Exists)
+                    {
+                        retval = false;
+                        break;
+                    }
+                }
+            }
+
+            return retval;
+        }
+
         /// <summary>
         /// Reads a files contents and returns a UTF encoded string
         /// </summary>
