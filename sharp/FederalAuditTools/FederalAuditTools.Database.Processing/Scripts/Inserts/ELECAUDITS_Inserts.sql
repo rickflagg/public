cecabc94871c262db8b9e31d8f6247bc6af71e21
Rick Flagg <rick.flagg@gmail.com>
2014-10-14 04:05:20 -0400

    Adding test data

---
+++ sharp/FederalAuditTools/FederalAuditTools.Database.Processing/Scripts/Inserts/ELECAUDITS_Inserts.sql
@@ -0,0 +1,207 @@
+
+-------------------------------------------------------------------------
+-- Report Id - 700000
+-- Audit Year - 2014
+-- Auditee - .NET Training Academy
+-------------------------------------------------------------------------
+INSERT INTO ELECAUDITS (
+   AUDITYEAR, REPORTID, CFDASEQNUM, 
+   CFDA, FEDERALPROGRAMNAME, AMOUNT, 
+   MAJORPROGRAM, TYPEREQUIREMENT, QCOSTS, 
+   FINDINGS, FINDINGREFNUMS, PASS_FLAG, 
+   FINAL, RD, DIRECT, 
+   CFDA_PREFIX, CFDA_EXT, EIN, 
+   DUNS, TYPEREPORT_MP, VERSION, 
+   ARRA, LOANS, ELECAUDITSID, 
+   FINDINGSCOUNT) 
+VALUES ( /* AUDITYEAR */,
+ 70000/* REPORTID */,
+ 001/* CFDASEQNUM */,
+ 93.001/* CFDA */,
+ 'Test Federal Program 1'/* FEDERALPROGRAMNAME */,
+ 654654/* AMOUNT */,
+ /* MAJORPROGRAM */,
+ /* TYPEREQUIREMENT */,
+ /* QCOSTS */,
+ /* FINDINGS */,
+ /* FINDINGREFNUMS */,
+ /* PASS_FLAG */,
+ /* FINAL */,
+ /* RD */,
+ /* DIRECT */,
+ /* CFDA_PREFIX */,
+ /* CFDA_EXT */,
+ /* EIN */,
+ /* DUNS */,
+ /* TYPEREPORT_MP */,
+ /* VERSION */,
+ /* ARRA */,
+ /* LOANS */,
+ 40000/* ELECAUDITSID */,
+ 1/* FINDINGSCOUNT */ );
+
+
+ -------------------------------------------------------------------------
+-- Report Id - 700001
+-- Audit Year - 2014
+-- Auditee - School of Javascript Implementation
+-------------------------------------------------------------------------
+INSERT INTO ELECAUDITS (
+   AUDITYEAR, REPORTID, CFDASEQNUM, 
+   CFDA, FEDERALPROGRAMNAME, AMOUNT, 
+   MAJORPROGRAM, TYPEREQUIREMENT, QCOSTS, 
+   FINDINGS, FINDINGREFNUMS, PASS_FLAG, 
+   FINAL, RD, DIRECT, 
+   CFDA_PREFIX, CFDA_EXT, EIN, 
+   DUNS, TYPEREPORT_MP, VERSION, 
+   ARRA, LOANS, ELECAUDITSID, 
+   FINDINGSCOUNT) 
+VALUES ( /* AUDITYEAR */,
+ /* REPORTID */,
+ /* CFDASEQNUM */,
+ /* CFDA */,
+ 'Test Federal Program 2'/* FEDERALPROGRAMNAME */,
+ 654645/* AMOUNT */,
+ /* MAJORPROGRAM */,
+ /* TYPEREQUIREMENT */,
+ /* QCOSTS */,
+ /* FINDINGS */,
+ /* FINDINGREFNUMS */,
+ /* PASS_FLAG */,
+ /* FINAL */,
+ /* RD */,
+ /* DIRECT */,
+ /* CFDA_PREFIX */,
+ /* CFDA_EXT */,
+ /* EIN */,
+ /* DUNS */,
+ /* TYPEREPORT_MP */,
+ /* VERSION */,
+ /* ARRA */,
+ /* LOANS */,
+ /* ELECAUDITSID */,
+ 2/* FINDINGSCOUNT */ );
+
+ -------------------------------------------------------------------------
+-- Report Id - 700002
+-- Audit Year - 2014
+-- Auditee - University of Java Best Practices
+-------------------------------------------------------------------------
+INSERT INTO ELECAUDITS (
+   AUDITYEAR, REPORTID, CFDASEQNUM, 
+   CFDA, FEDERALPROGRAMNAME, AMOUNT, 
+   MAJORPROGRAM, TYPEREQUIREMENT, QCOSTS, 
+   FINDINGS, FINDINGREFNUMS, PASS_FLAG, 
+   FINAL, RD, DIRECT, 
+   CFDA_PREFIX, CFDA_EXT, EIN, 
+   DUNS, TYPEREPORT_MP, VERSION, 
+   ARRA, LOANS, ELECAUDITSID, 
+   FINDINGSCOUNT) 
+VALUES ( /* AUDITYEAR */,
+ /* REPORTID */,
+ /* CFDASEQNUM */,
+ /* CFDA */,
+ 'Test Federal Program 3'/* FEDERALPROGRAMNAME */,
+ 7987987/* AMOUNT */,
+ /* MAJORPROGRAM */,
+ /* TYPEREQUIREMENT */,
+ /* QCOSTS */,
+ /* FINDINGS */,
+ /* FINDINGREFNUMS */,
+ /* PASS_FLAG */,
+ /* FINAL */,
+ /* RD */,
+ /* DIRECT */,
+ /* CFDA_PREFIX */,
+ /* CFDA_EXT */,
+ /* EIN */,
+ /* DUNS */,
+ /* TYPEREPORT_MP */,
+ /* VERSION */,
+ /* ARRA */,
+ /* LOANS */,
+ /* ELECAUDITSID */,
+ 3/* FINDINGSCOUNT */ );
+
+
+ -------------------------------------------------------------------------
+-- Report Id - 700003
+-- Audit Year - 2014
+-- Auditee - Python Bootcamp
+-------------------------------------------------------------------------
+INSERT INTO ELECAUDITS (
+   AUDITYEAR, REPORTID, CFDASEQNUM, 
+   CFDA, FEDERALPROGRAMNAME, AMOUNT, 
+   MAJORPROGRAM, TYPEREQUIREMENT, QCOSTS, 
+   FINDINGS, FINDINGREFNUMS, PASS_FLAG, 
+   FINAL, RD, DIRECT, 
+   CFDA_PREFIX, CFDA_EXT, EIN, 
+   DUNS, TYPEREPORT_MP, VERSION, 
+   ARRA, LOANS, ELECAUDITSID, 
+   FINDINGSCOUNT) 
+VALUES ( /* AUDITYEAR */,
+ /* REPORTID */,
+ /* CFDASEQNUM */,
+ /* CFDA */,
+ 'Test Federal Program 4'/* FEDERALPROGRAMNAME */,
+ 987897/* AMOUNT */,
+ /* MAJORPROGRAM */,
+ /* TYPEREQUIREMENT */,
+ /* QCOSTS */,
+ /* FINDINGS */,
+ /* FINDINGREFNUMS */,
+ /* PASS_FLAG */,
+ /* FINAL */,
+ /* RD */,
+ /* DIRECT */,
+ /* CFDA_PREFIX */,
+ /* CFDA_EXT */,
+ /* EIN */,
+ /* DUNS */,
+ /* TYPEREPORT_MP */,
+ /* VERSION */,
+ /* ARRA */,
+ /* LOANS */,
+ /* ELECAUDITSID */,
+ 4/* FINDINGSCOUNT */ );
+
+ -------------------------------------------------------------------------
+-- Report Id - 700004
+-- Audit Year - 2014
+-- Auditee - Linux Free Software Organization
+-------------------------------------------------------------------------
+INSERT INTO ELECAUDITS (
+   AUDITYEAR, REPORTID, CFDASEQNUM, 
+   CFDA, FEDERALPROGRAMNAME, AMOUNT, 
+   MAJORPROGRAM, TYPEREQUIREMENT, QCOSTS, 
+   FINDINGS, FINDINGREFNUMS, PASS_FLAG, 
+   FINAL, RD, DIRECT, 
+   CFDA_PREFIX, CFDA_EXT, EIN, 
+   DUNS, TYPEREPORT_MP, VERSION, 
+   ARRA, LOANS, ELECAUDITSID, 
+   FINDINGSCOUNT) 
+VALUES ( /* AUDITYEAR */,
+ /* REPORTID */,
+ /* CFDASEQNUM */,
+ /* CFDA */,
+ 'Test Federal Program 5'/* FEDERALPROGRAMNAME */,
+ 975454/* AMOUNT */,
+ /* MAJORPROGRAM */,
+ /* TYPEREQUIREMENT */,
+ /* QCOSTS */,
+ /* FINDINGS */,
+ /* FINDINGREFNUMS */,
+ /* PASS_FLAG */,
+ /* FINAL */,
+ /* RD */,
+ /* DIRECT */,
+ /* CFDA_PREFIX */,
+ /* CFDA_EXT */,
+ /* EIN */,
+ /* DUNS */,
+ /* TYPEREPORT_MP */,
+ /* VERSION */,
+ /* ARRA */,
+ /* LOANS */,
+ /* ELECAUDITSID */,
+ 5/* FINDINGSCOUNT */ );
